2b6c5effaee34d497669d3a9b07893f271c9e706
Liwei Feng <liweeiif@gmail.com>
2023-02-11 15:45:20 -0700

    setup gh-pages

---
+++ build/precache-manifest.fd91a46ba307d283dc4e5247b22d5418.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff89ca71714c347df1b154959ce159e7",
+    "url": "/React-Web-Forest/index.html"
+  },
+  {
+    "revision": "596d2f35ee140766ddd8",
+    "url": "/React-Web-Forest/static/css/main.9a0cae38.chunk.css"
+  },
+  {
+    "revision": "45fd5836b82a21efa67c",
+    "url": "/React-Web-Forest/static/js/2.d8eb43be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React-Web-Forest/static/js/2.d8eb43be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596d2f35ee140766ddd8",
+    "url": "/React-Web-Forest/static/js/main.60e5c160.chunk.js"
+  },
+  {
+    "revision": "0678c3cfece45c3ec388",
+    "url": "/React-Web-Forest/static/js/runtime-main.7bfe300e.js"
+  }
+]);
